040d491deaabf9c51b94a115b9061579483f1ecf
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-08-18 22:44:27 +0900

    style(): lessen padding for each article

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _buildArticle(Article article) {
     return Padding(
       key: Key(article.text),
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(4.0),
       child: ExpansionTile(
         children: [
           Row(
