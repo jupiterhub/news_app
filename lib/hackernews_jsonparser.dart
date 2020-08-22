9ec55903edd328cf5fe7b9ec1694b2c1771b3bce
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-08-22 09:15:08 +0900

    test(): added unit test for parsing json in hackernews

---
+++ lib/hackernews_jsonparser.dart
@@ -0,0 +1,9 @@
+import 'package:news_app/src/article.dart';
+
+List<int> parseStories(String jsonStr) {
+  throw UnimplementedError();
+}
+
+Article parseArticle(String jsonStr) {
+  throw UnimplementedError();
+}
