4b1a280d6a8929f23fb4ef342f35e76d16330ec6
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-09-02 21:59:49 +0900

    feat: added pulling aticle data directly from network

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/hackernews_jsonparser.dart';
 import 'package:news_app/src/article.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -31,25 +33,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Article> _articles = articles;
+  List<int> _ids = [
+    24322861,
+    24341535,
+    24343672,
+    24323589,
+    24334731,
+    24341867,
+    24324974,
+    24332412,
+    24343361,
+    24330326,
+    24344045,
+    24338152,
+    24343572,
+    24342540,
+    24332485,
+    24331698
+  ];
+
+  Future<Article> _getArticle(int id) async {
+    final itemEndpoint = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
+    var itemResponse = await http.get(itemEndpoint);
+
+    if (itemResponse.statusCode == 200) {
+      return parseArticle(itemResponse.body);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: RefreshIndicator(
-        child: Center(
-            child: ListView(children: _articles.map(_buildArticle).toList())),
-        onRefresh: () async {
-          await Future.delayed(Duration(seconds: 1), () {
-            setState(() {
-              _articles.removeAt(0);
-            });
-          });
-        },
-      )
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ListView(
+          children:
+          _ids.map((id) =>
+              FutureBuilder<Article>(
+                future: _getArticle(id),
+                builder: (BuildContext context,
+                    AsyncSnapshot<Article> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: _buildArticle(snapshot.data),
+                    );
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                },
+              )).toList(),
+        )
     );
   }
 
